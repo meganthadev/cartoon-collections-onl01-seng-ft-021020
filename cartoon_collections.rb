2c9162c5385188e84e92876be612d9d3ae3d8c25
Megan Horsfield <meganthadev@gmail.com>
2020-02-20 06:11:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,11 +2,13 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
 end
 
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
 end
 
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+short_words = ["puff", "go", "two"]
 
 def long_planeteer_calls(short_words)
   # Your code here
