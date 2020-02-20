29b94e2d35ff5bfdb17266d1380eb15bffe7d84d
Megan Horsfield <meganthadev@gmail.com>
2020-02-20 06:21:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,8 @@ ingredients = ["garlic", "rosemary", "bread"]
 
 def find_the_cheese(snacks)
   snacks.incude? { |item|
-  
+   cheese_types.include?(item)
+  end
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
