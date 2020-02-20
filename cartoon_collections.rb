f695ed37d8da008544a05442be5f137908af8e3f
Megan Horsfield <meganthadev@gmail.com>
2020-02-20 06:26:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,12 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
-
-def roll_call_dwarves(array)
-  i = 0
-  array.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
 end
 
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
 end
-
-summon_captain_planet(planeteer_calls)
 
 short_words = ["puff", "go", "two"]
 
